dafe39354b706de010b01d4c4ba3f32fb9055a05
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-03 10:55:55 +0100

    Correção de hrefs

--- assets/files/logout.php
+++ assets/files/logout.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="/stylesheet.css">
     <title>BV | Logout</title>
 </head>
 
