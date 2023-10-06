7da82c53262f7941db350575560dc3ab483d2587
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-06 11:25:36 +0100

    Criadas novas páginas para o veterinário e adicionadas à navBar

--- assets/files/navbar.php
+++ assets/files/navbar.php
@@ -1,6 +1,8 @@
 <div class="navBar">
         <a class="textNavBar" href="informacoesanimal.php">Vacinas</a>
         <a class="textNavBar" href="associacoes.php">Associações</a>
+        <a class="textNavBar" href="escolheranimal.php" <?php if($_SESSION['tipoUser'] != 'vet'){echo 'hidden';}?>>Escolher Animal</a>
+        <a class="textNavBar" href="animais.php"<?php if($_SESSION['tipoUser'] != 'vet'){echo 'hidden';}?>>Animais</a>
         <a class="textNavBar" href="#" id="logout">Log out</a>
 </div>
 
