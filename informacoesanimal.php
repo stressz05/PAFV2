df0d85e79b1e06e45f43c812acb2a925c9ae92c6
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-06 10:41:52 +0100

    Adicionado viewport para tornar o site ser mais responsivo em dispositivos móveis

--- informacoesanimal.php
+++ informacoesanimal.php
@@ -8,6 +8,7 @@
 <head>
     <title>BV | Informações</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 
--- registarform.php
+++ registarform.php
@@ -4,6 +4,7 @@
 <head>
     <title>BV | Registo</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 
