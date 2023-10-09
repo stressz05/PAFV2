8fb8ad6d2bd96aeb2b8bc3e7547418f4ff718658
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-09 12:37:47 +0100

    Primeira versão da página "Escolher Animal"

--- escolheranimal.php
+++ escolheranimal.php
@@ -1,12 +1,12 @@
-<?php 
-    require "assets/files/verificacao.php";
+<?php
+require "assets/files/verificacao.php";
 
-    if(isset($_SESSION['loggedin'])){
-        if($_SESSION['tipoUser'] === 'dono'){
-            header("Location: assets/files/acessonegado.php");
-            exit;
-        }
+if (isset($_SESSION['loggedin'])) {
+    if ($_SESSION['tipoUser'] === 'dono') {
+        header("Location: assets/files/acessonegado.php");
+        exit;
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,9 +18,66 @@
     <link rel="stylesheet" href="/stylesheet.css">
     <title>BV / Escolher</title>
 </head>
+
 <body class="body">
     <?php include "assets/files/navbar.php" ?>
-    <p>ESCOLHER</p>
+    <div class="centered">
+        <table class="table">
+            <tr>
+                <th class="tableHeader">ID</th>
+                <th class="tableHeader">Nome</th>
+                <th class="tableHeader">Raça</th>
+                <th class="tableHeader">Família</th>
+                <th class="tableHeader">Idade</th>
+                <th class="tableHeader">Peso</th>
+                <th class="tableHeader">Tamanho</th>
+                <th class="tableHeader">Género</th>
+                <th class="tableHeader">NIF do Dono</th>
+                <th class="tableHeader">Nome Dono</th>
+            </tr>
+            <?php
+            require "assets/files/conexao.php";
+
+            $sql = "SELECT * FROM animal";
+            $sql_query = $conn->query($sql);
+
+            if ($sql_query !== false) {
+                while ($linhas = $sql_query->fetch_assoc()) {
+                    $nif_dono = $linhas['NIF_Dono'];
+                    $sql = "SELECT Nome_Dono FROM dono WHERE NIF_Dono = $nif_dono";
+                    $sql_query_dono = $conn->query($sql);
+
+                    if ($sql_query_dono !== false) {
+                        $linhas_dono = $sql_query_dono->fetch_assoc();
+                        $nome_dono = $linhas_dono['Nome_Dono'];
+                    }
+
+                    echo "<tr>";
+                    echo "<td class='tableRows'>" . $linhas['ID_Animal'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['Nome'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['Raca'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['Tipo_Animal'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['Idade'] . " " . $linhas['Idade_Medida'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['Peso'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['Tamanho'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['Genero'] . "</td>";
+                    echo "<td class='tableRows'>" . $linhas['NIF_Dono'] . "</td>";
+                    echo "<td class='tableRows'>" . $nome_dono . "</td>";
+                    echo "</tr>";
+                }
+            }
+            ?>
+        </table>
+    </div>
+
+    <div class="centered">
+        <form action="#" class="form-center">
+            <p class="label">Escolha o ID de um animal: </p>
+            <input type="number" class="input" name="ID_animal" id="idanimal" placeholder="ex. 1">
+            <input type="button" value="Escolher" class="btnEscolherAnimal">
+        </form>
+    </div>
     <?php include "assets/files/footer.php" ?>
 </body>
+
 </html>
