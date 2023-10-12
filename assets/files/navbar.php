8445cd6193a2932a36d8a5d8b11eed5a1f16393a
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-12 10:19:40 +0100

    de "Log out" para "Logout"

--- assets/files/navbar.php
+++ assets/files/navbar.php
@@ -3,7 +3,7 @@
         <a class="textNavBar" href="associacoes.php">Associações</a>
         <a class="textNavBar" href="escolheranimal.php" <?php if($_SESSION['tipoUser'] != 'vet'){echo 'hidden';}?>>Escolher Animal</a>
         <a class="textNavBar" href="animais.php"<?php if($_SESSION['tipoUser'] != 'vet'){echo 'hidden';}?>>Animais</a>
-        <a class="textNavBar" href="#" id="logout">Log out</a>
+        <a class="textNavBar" href="#" id="logout">Logout</a>
 </div>
 
 <script>
