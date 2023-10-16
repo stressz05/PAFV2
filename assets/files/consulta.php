c4cd00906f418ddb5026dea7492ec4c29accc40e
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-16 12:36:05 +0100

    Finalização do Painel das consultas

---
+++ assets/files/consulta.php
@@ -0,0 +1,35 @@
+<head>
+    <title>BV / Consulta</title>
+</head>
+
+<?php
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    require "conexao.php";
+
+    $idConsulta = $_SESSION['idConsulta'];
+    $idAnimal = $_SESSION['idAnimal'];
+
+    $nomeVac = $_POST['nomeVac'];
+    $dataVac = $_POST['dataVac']; 
+    $descVac = $_POST['descVac'];
+    $peso = $_POST['peso'];
+    $obs = $_POST['obs'];
+
+    $sql = "INSERT INTO consulta (ID_Animal, ID_Consulta, Nome_Vacina, Descricao_Vacina, Peso, Data, Observacoes) VALUES ('$idAnimal', '$idConsulta', '$nomeVac', '$descVac', '$peso', '$dataVac', '$obs')";
+
+    if($conn->query($sql) === TRUE){
+        $sqlUpdate = "UPDATE animal SET Peso = '$peso' WHERE ID_Animal = '$idAnimal'";
+        if($conn->query($sqlUpdate) === TRUE){
+            echo 'Valores inseridos com sucesso.';
+        }
+    } else {
+        echo 'Ups! Erro...' . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+
+
+?>
--- painelCon.php
+++ painelCon.php
@@ -31,45 +31,67 @@ if (isset($_SESSION['loggedin'])) {
     $sql = "SELECT Nome, Peso, ID_Consulta FROM animal WHERE ID_Animal = $id";
     $sql_query = $conn->query($sql);
 
-    if($sql_query !== false){
+    if ($sql_query !== false) {
         $rows = $sql_query->fetch_assoc();
         $nome = $rows['Nome'];
         $peso = $rows['Peso'];
         $idCon = $rows['ID_Consulta'];
+
+        $_SESSION['idAnimal'] = $id;
+        $_SESSION['idConsulta'] = $idCon;
     }
     ?>
+    <form action="assets/files/consulta.php" method="post">
+        <div class="centered" style="margin-top: 0">
+            <div class="containerPainel">
+                <div class="left">
+                    <p class="pBlocks"><strong>Nome: </strong><?php echo $nome; ?></p>
+                    <div class="pBlocks">
+                        <label>
+                            <strong>Nome da Vacina: </strong>
+                            <br>
+                            <input class="input" name="nomeVac" type="text">
+                        </label>
+                    </div>      
+                    <div class="pBlocks">
+                        <label>
+                            <strong>Data de administração: </strong>
+                            <br>
+                            <input class="input" name="dataVac" type="date">
+                        </label>
+                    </div>
+                    <div class="pBlocks">
+                        <label>
+                            <strong>Descrição da vacina: </strong>
+                            <span style="font-size: 15px; color: green">(opcional)</span>
+                            <br>
+                            <textarea class="textArea" name="descVac"></textarea>
+                        </label>
+                    </div>
+                </div>    
 
-    <div class="centered" style="margin-top: 0">
-        <div class="containerPainel">
-            <div class="left">
-                <p class="pBlocks"><strong>Nome: </strong><?php echo $nome; ?></p>
-                <div class="pBlocks">
-                    <label><strong>Nome da Vacina: </strong><br><input type="text" class="input"></label>
-                </div>
-                <div class="pBlocks">
-                    <label><strong>Data de administração: </strong><br><input type="date" class="input"></label>
-                </div>
-                <div class="pBlocks">
-                    <label><strong>Descrição da vacina: </strong><span style="font-size: 15px; color: green">(opcional)</span><br><textarea class="textArea"></textarea></label>
-                </div>
-            </div>
-            
-            <div class="right">
-                <div class="pBlocks">
-                    <label><strong>Peso: </strong><br><input type="text" class="input" placeholder=" Peso anterior: <?php echo $peso; ?>Kg"></label>
-                </div>
-                <div class="pBlocks">
-                    <label><strong>Observações: </strong><br><textarea class="textArea" style="height: 240px"></textarea></label>
+                <div class="right">
+                    <div class="pBlocks">
+                        <label>
+                            <strong>Peso: </strong>
+                            <br>
+                            <input class="input" type="text" name="peso" placeholder=" Peso anterior: <?php echo $peso; ?>Kg">
+                        </label>
+                    </div>
+                    <div class="pBlocks">
+                        <label>
+                            <strong>Observações: </strong>
+                            <br>
+                            <textarea class="textArea" name="obs" style="height: 240px"></textarea>
+                        </label>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="centered" style="margin-top: 20px">
-        <form action="animais.php" method="post" class="formEscolher">
-            <input type="submit" value="Confirmar" class="btnEscolherAnimal">
-        </form>
-    </div>
+        <div class="centered" style="margin-top: 10px">
+            <button class="btnEscolherAnimal" type="submit">Confirmar</button>
+        </div>
+    </form>
 
     <?php include "assets/files/footer.php" ?>
 </body>
