75e49286a951c60c4c0f2cbb6ac623468fac747d
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-17 14:06:26 +0100

    Adicionado um IF para a descrição não ser colocada caso esteja vazia

--- informacoesanimal.php
+++ informacoesanimal.php
@@ -1,9 +1,9 @@
 <?php
-    require "assets/files/verificacao.php";
+require "assets/files/verificacao.php";
 
-    if($_SESSION['tipoUser'] == 'vet'){
-        header("Location: /escolheranimal.php");
-    }
+if ($_SESSION['tipoUser'] == 'vet') {
+    header("Location: /escolheranimal.php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -17,61 +17,97 @@
 </head>
 
 <body class="body" onload="boasVindas()">
-    <!--* Cria a barra de navegação do Site-->
+    <!-- Cria a barra de navegação do Site-->
     <?php include "assets/files/navbar.php"; ?>
-   
+
     <div class="boasVindas">
-        <p><strong>Olá</strong> <?php ($_SESSION['tipoUser'] === 'dono') ? print($_SESSION['nome_dono']) : print($_SESSION['nome_vet']);?>!</p>
+        <p><strong>Olá</strong> <?php ($_SESSION['tipoUser'] === 'dono') ? print($_SESSION['nome_dono']) : print($_SESSION['nome_vet']); ?>!</p>
     </div>
 
-    <!--! Cria uma pequena área que irá conter as informações do animal-->
-    <!--! Ainda vai levar alterações quando se ligar à base de dados-->
-
+    <!-- Cria uma pequena área que irá conter as informações do animal-->
     <div class="infoP">
         <p>Informações básicas do animal: </p>
     </div>
+    
     <div class="info">
-        <?php 
-            require "assets/files/conexao.php";
-            $nif = $_SESSION['nif_dono'];
-            
-            $sql = "SELECT Nome, Idade, Idade_Medida, Tamanho, Peso, ID_Animal, ID_Consulta FROM animal WHERE NIF_Dono = '$nif'";
-            $query_sql = $conn->query($sql);
+        <?php
+        require "assets/files/conexao.php";
+        $nif = $_SESSION['nif_dono'];
 
-            if($query_sql == true){
-                $linhas = $query_sql->fetch_assoc();
-                
-                $nome = $linhas['Nome'];
-                $idade = $linhas['Idade'];
-                $idadeTipo = $linhas['Idade_Medida'];
-                $tamanho = $linhas['Tamanho'];
-                $peso = $linhas['Peso'];
-            }
-            
-            echo "<form action='assets/files/editar.php' method='post'>";
-            echo "<img id='edit' class='edit' src='assets/imagens/editSquare.png' title='Editar' onclick='editar()'>";
-            echo "<img id='cancel' class='edit' src='assets/imagens/cancel.png 'title='Cancelar' onclick='cancelar()' hidden>";
-            echo "<p id='name'><strong>Nome:</strong> " . $nome . "</p>";
-            echo "<p id='age'><strong>Idade:</strong> " . "<span id='ageV'>" . $idade . " " . $idadeTipo . "</span>". "<input class='editarInput' type='number' name='ageInput' id='ageInput' hidden>";
-            echo "<label id='labelMonth' hidden>|<input type='checkbox' name='typeAge' value='month' id='month'>Meses</label>";
-            echo "<label id='labelYear' hidden><input type='checkbox' name='typeAge' value='year' id='year'>Anos</label></p>";
-            echo "<p id='size'><strong>Tamanho:</strong> " . $tamanho .  "</p>";
-            echo "<p id='weight'><strong>Peso:</strong> " . "<span id='weightV'>" . $peso . "Kg" . "</span>". "<input class='editarInput' type='text' name='weightInput' id='weightInput' hidden>";
-            echo "<input type='submit' class='btnEscolherAnimal' value='Confirmar' id='confirmar' style='float: right;' hidden></input></p>";
-            echo "</form>";
+        $sql = "SELECT Nome, Idade, Idade_Medida, Tamanho, Peso, ID_Animal, ID_Consulta FROM animal WHERE NIF_Dono = '$nif'";
+        $query_sql = $conn->query($sql);
+
+        if ($query_sql == true) {
+            $linhas = $query_sql->fetch_assoc();
+
+            $nome = $linhas['Nome'];
+            $idade = $linhas['Idade'];
+            $idadeTipo = $linhas['Idade_Medida'];
+            $tamanho = $linhas['Tamanho'];
+            $peso = $linhas['Peso'];
+        }
+
+        echo "<form action='assets/files/editar.php' method='post'>";
+        echo "<img id='edit' class='edit' src='assets/imagens/editSquare.png' title='Editar' onclick='editar()'>";
+        echo "<img id='cancel' class='edit' src='assets/imagens/cancel.png 'title='Cancelar' onclick='cancelar()' hidden>";
+        echo "<p id='name'><strong>Nome:</strong> " . $nome . "</p>";
+        echo "<p id='age'><strong>Idade:</strong> " . "<span id='ageV'>" . $idade . " " . $idadeTipo . "</span>" . "<input class='editarInput' type='number' name='ageInput' id='ageInput' hidden>";
+        echo "<label id='labelMonth' hidden>|<input type='checkbox' name='typeAge' value='month' id='month'>Meses</label>";
+        echo "<label id='labelYear' hidden><input type='checkbox' name='typeAge' value='year' id='year'>Anos</label></p>";
+        echo "<p id='size'><strong>Tamanho:</strong> " . $tamanho .  "</p>";
+        echo "<p id='weight'><strong>Peso:</strong> " . "<span id='weightV'>" . $peso . "Kg" . "</span>" . "<input class='editarInput' type='text' name='weightInput' id='weightInput' hidden>";
+        echo "<input type='submit' class='btnEscolherAnimal' value='Confirmar' id='confirmar' style='float: right;' hidden></input></p>";
+        echo "</form>";
         ?>
-
-
     </div>
 
+    <!-- Cria a area que vai conter as vacinas que o animal levou-->
     <div class="infoP">
         <p>Vacinas do Animal:</p>
     </div>
+    <div class="centered">
+        <div class="block">
+            <?php 
+            require "assets/files/conexao.php";
 
+            $nif = $_SESSION['nif_dono'];
+            $sqlID = "SELECT ID_Animal, ID_Consulta FROM animal WHERE NIF_Dono = '$nif'";
+            $sqlID_query = $conn->query($sqlID);
 
+            if($sqlID_query == true){
+                $linhasID = $sqlID_query->fetch_assoc();
+                $id = $linhasID['ID_Animal'];
+                $idCon = $linhasID['ID_Consulta'];
+            }
+
+            $sql = "SELECT Nome_Vacina, Descricao_Vacina, Peso, Data, Observacoes FROM consulta WHERE ID_Animal = '$id' AND ID_Consulta = '$idCon'";
+            $sql_query = $conn->query($sql);
+            
+            if($sql_query == true){
+                while($linhas = $sql_query->fetch_assoc()){
+                    $nomeVac = $linhas['Nome_Vacina'];
+                    $descVac = $linhas['Descricao_Vacina'];
+                    $peso = $linhas['Peso'];
+                    $data = $linhas['Data'];
+                    $obs = $linhas['Observacoes'];
+
+                    echo "<div class=b-row style='height: fit-content'>";
+                    echo "<strong>Vacina: </strong>" . $nomeVac . "<br>";
+                    if($descVac != ''){
+                        echo "<strong>Descrição: </strong><br>" . $descVac . "<br>";
+                    }
+                    echo "<strong>Peso: </strong>" . $peso . "Kg" . "<br>";
+                    echo "<strong>Data: </strong>" . $data . "<br>";
+                    echo "<strong>Observações: </strong><br>" . $obs;
+                    echo "</div>";
+                }
+            }
+            ?>
+        </div>
+    </div>
+
+    <?php include "assets/files/footer.php"; ?>
 </body>
-
-<?php include "assets/files/footer.php";?>
 
 <script>
     //. Função para se puder editar.
@@ -100,7 +136,7 @@
     }
 
     //. Função para cancelar a Edição
-    function cancelar(){
+    function cancelar() {
         let age = document.getElementById('ageInput');
         let weight = document.getElementById('weightInput');
         let confirmar = document.getElementById('confirmar');
