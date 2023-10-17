57518750780323ad7771c4e691208927afdc774c
Afonso Almeida <a.sil.almeida@gmail.com>
2023-10-17 10:40:01 +0100

    Adicionada verificação para ver se o anima já foi escolhido, tamanho da letra aumentado

--- assets/files/escolhaAnimal.php
+++ assets/files/escolhaAnimal.php
@@ -8,19 +8,28 @@ if (!isset($_SESSION)) {
 $id = $_POST['ID_animal'];
 
 //. Verifica se o ID existe na base de dados
-$sql_check = "SELECT ID_Animal FROM animal WHERE ID_Animal = '$id'";
+$sql_check = "SELECT ID_Animal, NIF_Vet FROM animal WHERE ID_Animal = '$id'";
 $result_check = $conn->query($sql_check);
 
 if ($result_check->num_rows > 0) {
-    //. O ID existe na base de dados, então o NIF do veterinário é inserido na base de dados
-    $sql = "UPDATE animal SET NIF_Vet = '$nifVet' WHERE ID_Animal = '$id'";
+    $row = $result_check->fetch_assoc();
+    $nif = $row['NIF_Vet'];
+    if($nif != 0){
+        //. O ID existe na base de dados e já foi escolhido por outro veterinário
+        echo "<div style='color: red; font-size: 22px;'>Este animal já foi escolhido! Por favor escolha outro.</div>";
+        echo "<a style='font-size: 20px;' href='/escolheranimal.php'>Voltar atrás</a>";
+        exit;
+    } else {
+        //. O ID existe na base de dados, então o NIF do veterinário é inserido na base de dados
+        $sql = "UPDATE animal SET NIF_Vet = '$nifVet' WHERE ID_Animal = '$id'";
+    }
 
     if ($conn->query($sql) === TRUE) {
-        echo "<div style='color: green;'>Animal guardado com sucesso.</div>";
-        echo "<a href='/escolheranimal.php'>Voltar atrás</a>";
+        echo "<div style='color: green; font-size: 22px;'>Animal guardado com sucesso.</div>";
+        echo "<a font-size: 20px; href='/escolheranimal.php'>Voltar atrás</a>";
     } else {
-        echo "<div style='color: red;'>Ups! Alguma coisa correu mal :(</div>" . "<br>" . $conn->error;
-        echo "<a href='/escolheranimal.php'>Voltar atrás</a>";
+        echo "<div style='color: red; font-size: 22px;'>Ups! Alguma coisa correu mal :(</div>" . "<br>" . $conn->error;
+        echo "<a font-size: 22px;href='/escolheranimal.php'>Voltar atrás</a>";
     }
 } else {
     //! O ID não existe na base de dados
@@ -28,4 +37,4 @@ if ($result_check->num_rows > 0) {
     echo "Por favor confirme se escolheu o ID certo." . "<br>";
     echo "<a href='/escolheranimal.php'>Voltar atrás</a>";
 }
-?>
+?>  
