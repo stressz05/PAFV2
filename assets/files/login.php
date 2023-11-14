26fda92ec9147722bf8228abaf8ae057906514e2
Afonso Almeida <a.sil.almeida@gmail.com>
2023-11-14 14:14:34 +0000

    Código de login refeito.

--- assets/files/login.php
+++ assets/files/login.php
@@ -16,93 +16,74 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if(!isset($_SESSION)){
             session_start();
             $_SESSION['loggedin'] = true;
-        }
-        $_SESSION['tipoUser'] = 'admin';
-    }
-
-
-    $sqlPass = "SELECT Password_Dono FROM dono WHERE NIF_Dono = '$nif'";
-    $sql_query = $conn->query($sqlPass) or die("Alguma coisa correu mal..." . "<br>" . $conn->error);
-    $numPasswords = $sql_query->num_rows;
-
-
-    //! Procura e testa as passwords
-    if ($numPasswords == 1) {
-        $row = $sql_query->fetch_assoc();
-        $passwordhash = $row['Password_Dono'];
-        $passwordFinal = password_verify($password2, $passwordhash);
-    } else {
-        $sqlPass = "SELECT Password_Vet FROM veterinario WHERE NIF_Vet = '$nif'";
-        $sql_query = $conn->query($sqlPass) or die("Alguma coisa correu mal..." . "<br>" . $conn->error);
-        $numPasswords = $sql_query->num_rows;
-        if ($numPasswords == 1) {
-            $row = $sql_query->fetch_assoc();
-            $passwordhash = $row['Password_Vet'];
-            $passwordFinal = password_verify($password2, $passwordhash);
+            $_SESSION['tipoUser'] = 'admin';
         }
     }
 
-    //! Caso encontre a password efetua o login do utilizador
-    if ($passwordFinal == TRUE) {
+    $sqlDono = "SELECT * FROM dono WHERE NIF_Dono = $nif";
+    $sql_query = $conn->query($sqlDono);
 
-        //? Seleciona todos os valores onde existe o nif
-        $sql = "SELECT * FROM dono WHERE Nome_Dono = '$username' AND NIF_Dono = '$nif'";
+    $sqlVet = "SELECT * FROM veterinario WHERE NIF_Vet = $nif";
+    $sqlQuery = $conn->query($sqlVet);
 
-        //? Consulta a base de dados e armazena o seu resultado, caso corra mal exibe um aviso.
-        $sql_query = $conn->query($sql) or die("Alguma coisa correu mal..." . "<br>" . $conn->error);
+    if($sql_query == true && $sql_query->num_rows == 1){
+        $result = $sql_query->fetch_assoc();
+        $name = $result['Nome_Dono'];
+        $pwd = $result['Password_Dono'];
+        $nifBD = $result['NIF_Dono'];
+        
+        $pwdTest = password_verify($password2, $pwd);
 
-        //? Vê quantas linhas da DB a query devolveu
-        $numUsers = $sql_query->num_rows;
-
-        //? Testa a variavel "numUsers" e vê se o resultado é 1
-        if ($numUsers == 1) {
-            $row = $sql_query->fetch_assoc();
-
-            if(!isset($_SESSION)){ //? isset -> função do php que devolve true se uma variável estiver definida
-                session_start();
-                $_SESSION['loggedin'] = true;
-            }
-            
-            $_SESSION['tipoUser'] = 'dono';
-            $_SESSION['nif_dono'] = $row['NIF_Dono'];
-            $_SESSION['nome_dono'] = $row['Nome_Dono'];
-
-            header("Location: /informacoesanimal.php");
-        } else {
-            //? Seleciona todos os valores onde existe o nif e a password
-            $sql = "SELECT * FROM veterinario WHERE Nome_Vet = '$username' AND NIF_Vet = '$nif'";
-
-            //? Consulta a base de dados e armazena o seu resultado, caso corra mal exibe um aviso.
-            $sql_query = $conn->query($sql) or die("Alguma coisa correu mal..." . "<br>" . $conn->error);
-   
-            $numUsers = $sql_query->num_rows;
-
-            if ($numUsers == 1) {
-                $row = $sql_query->fetch_assoc();
-
-                $status = $row['status'];
-
-                if($status != 'aprovado'){
-                    header("Location: porAprovar.php");
-                    exit;
+        if($pwdTest == true){
+            if($username == $name){
+                if(!isset($_SESSION)){ //? isset -> função do php que devolve true se uma variável estiver definida
+                    session_start();
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['tipoUser'] = 'dono';
+                    $_SESSION['nif_dono'] = $nifBD;
+                    $_SESSION['nome_dono'] = $name;
                 }
-                
+                header("Location: /informacoesanimal.php");
+                exit;
+            } else {
+                echo "<p style='font-size:25px'>Nome incorreto. Por favor verifique.</p>";
+            }
+        } else {
+            echo "<p style='font-size:25px'>Password incorreta. Por favor verifique.</p>";
+        }
+    } else if($sqlQuery == true && $sqlQuery->num_rows == 1){
+        $resultVet = $sqlQuery->fetch_assoc();
+        $name = $resultVet['Nome_Vet'];
+        $pwd = $resultVet['Password_Vet'];
+        $nifBD = $resultVet['NIF_Vet'];
+        $status = $resultVet['status'];
+
+        if($status != "aprovado"){
+            header("Location: porAprovar.php");
+            exit;
+        }
+
+        $pwdTest = password_verify($password2, $pwd);
+
+        if($pwdTest == true){
+            if($username == $name){
                 if(!isset($_SESSION)){
                     session_start();
                     $_SESSION['loggedin'] = true;
+                    $_SESSION['tipoUser'] = 'vet';
+                    $_SESSION['nif_vet'] = $nifBD;
+                    $_SESSION['nome_vet'] = $name;
                 }
-
-                $_SESSION['tipoUser'] = 'vet';
-                $_SESSION['nif_vet'] = $row['NIF_Vet'];
-                $_SESSION['nome_vet'] = $row['Nome_Vet'];
-
                 header("Location: /escolheranimal.php");
+                exit;
             } else {
-                echo "Falha ao fazer login! Causa:" . "<br>Nif ou Password Incorretos!" . "<br>Nenhum Utilizador existe!";
+                echo "<p style='font-size:22px'>Nome incorreto. Por favor verifique.</p>";
             }
+        } else {
+            echo "<p style='font-size:22px'>Password incorreta. Por favor verifique.</p>";
         }
     } else {
-        echo "Password incorreta!" . "<br>Por favor verifique!";
+        echo "<p style='font-size:22px'>NIF incorreto. Por favor verifique.</p>";
     }
 }
 ?>
